aca72c14bb9b2ec2ea3dc493edba556d38ec9bf8
Benjamin Schweizer <bschweiz@gmail.com>
2020-11-28 09:29:00 -0600

    completed ch5, got the products rendering on the DOM, additionally i went in and created a CSS sheet for Products and Locations that matches the formatting of the Kennels project

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -2,13 +2,23 @@ import React from "react"
 // import { Route } from "react-router-dom"
 import { LocationProvider } from "../location/LocationProvider"
 import { LocationList } from "../location/LocationList"
+import { ProductProvider } from "../product/ProductProvider"
+import { ProductList } from "../product/ProductList"
 
 
 export const KandyKorner = (props) => {
     return (
+        <>
+        <h2>Locations</h2>
         <LocationProvider>
             <LocationList />
         </LocationProvider>
+        
+        <h2>Products</h2>
+        <ProductProvider>
+            <ProductList />
+        </ProductProvider>
+        </>
     )
 }
     
--- src/location/Location.js
+++ src/location/Location.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Location.css"
 
 
 export const Location = ({location}) => (
