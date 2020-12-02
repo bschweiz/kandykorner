2453bf454890e00503d4b5f887a276a6fdb7c9d0
Benjamin Schweizer <bschweiz@gmail.com>
2020-12-02 16:35:35 -0600

    got the Login and Register functionality working, on to ch10 now

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,15 +1,26 @@
 import React from "react"
-
+import { Route, Redirect } from "react-router-dom"
 import { NavBar } from "./NavBar"
 import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 
-export const KandyKorner = () => {
-    return (
-        <>
-            <NavBar />
-            <ApplicationViews />
-        </>
-    )
-}
-    
+export const KandyKorner = () => (               
+    <>
+        <Route render={() => {
+            if (localStorage.getItem("kandy_customer")) {
+                return (
+                    <>
+                        <Route render={props => <NavBar {...props} />} />
+                        <Route render={props => <ApplicationViews {...props} />} />
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+)
